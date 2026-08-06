1a191d428630a8623cc3bd605cf8649695fcbd36
cheenusaini47-design <cheenusaini47@gmail.com>
2026-08-06 15:41:44 +0530

    Time: 17 ms (16.63%), Space: 52.3 MB (97.43%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,16 @@
+
+import java.util.HashMap;
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> hm= new HashMap<>();
+        for(int i =0;i<nums.length;i++){
+            
+                hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+           if(hm.get(nums[i]) > nums.length/2){
+            return nums[i];
+           }
+            
+        }
+        return -1;
+    }
+}
